0228db46311d2adb3a0231360ef764e947233332
Ishi0324 <98327719+Ishi0324@users.noreply.github.com>
2025-05-03 18:04:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './component/Header';
+import Meme from "./component/Meme";
+
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Meme />
+   </div>
+    
+  );
+}
+
+export default App;
